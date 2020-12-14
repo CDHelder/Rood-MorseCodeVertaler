91d88bccd2737f33f915143f6c326df1a844b389
CDHelder <72396857+CDHelder@users.noreply.github.com>
2020-12-14 14:44:40 +0100

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,129 @@
+﻿
+namespace MorseCodeVertaler
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxNL = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxMC = new System.Windows.Forms.TextBox();
+            this.buttonVertalen = new System.Windows.Forms.Button();
+            this.buttonReset = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBoxNL
+            // 
+            this.textBoxNL.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxNL.Location = new System.Drawing.Point(12, 37);
+            this.textBoxNL.Multiline = true;
+            this.textBoxNL.Name = "textBoxNL";
+            this.textBoxNL.Size = new System.Drawing.Size(776, 172);
+            this.textBoxNL.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(122, 25);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Nederlands";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 238);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(125, 25);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Morse code";
+            // 
+            // textBoxMC
+            // 
+            this.textBoxMC.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxMC.Location = new System.Drawing.Point(12, 266);
+            this.textBoxMC.Multiline = true;
+            this.textBoxMC.Name = "textBoxMC";
+            this.textBoxMC.ReadOnly = true;
+            this.textBoxMC.Size = new System.Drawing.Size(776, 172);
+            this.textBoxMC.TabIndex = 2;
+            // 
+            // buttonVertalen
+            // 
+            this.buttonVertalen.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonVertalen.Location = new System.Drawing.Point(143, 215);
+            this.buttonVertalen.Name = "buttonVertalen";
+            this.buttonVertalen.Size = new System.Drawing.Size(146, 45);
+            this.buttonVertalen.TabIndex = 4;
+            this.buttonVertalen.Text = "Vertalen";
+            this.buttonVertalen.UseVisualStyleBackColor = true;
+            this.buttonVertalen.Click += new System.EventHandler(this.buttonVertalen_Click);
+            // 
+            // buttonReset
+            // 
+            this.buttonReset.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonReset.Location = new System.Drawing.Point(511, 215);
+            this.buttonReset.Name = "buttonReset";
+            this.buttonReset.Size = new System.Drawing.Size(146, 45);
+            this.buttonReset.TabIndex = 5;
+            this.buttonReset.Text = "Reset";
+            this.buttonReset.UseVisualStyleBackColor = true;
+            this.buttonReset.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.buttonReset);
+            this.Controls.Add(this.buttonVertalen);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBoxMC);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxNL);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxNL;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBoxMC;
+        private System.Windows.Forms.Button buttonVertalen;
+        private System.Windows.Forms.Button buttonReset;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MorseCodeVertaler
+{
+    public partial class Form1 : Form
+    {
+        private static Dictionary<char, string> VertalerNL;
+
+        public Form1()
+        {
+            InitializeComponent();
+            //Dictionary data
+            VertalerNL = new Dictionary<char, string>()
+            { {'a', ".-"}, {'b', "-..."}, {'c', "-.-."}, {'d', "-.."}, {'e', "."}, {'f', "..-."}, {'g', "--."},
+            {'h', "...."}, {'i', ".."}, {'j', ".---"}, {'k', "-.-"}, {'l', ".-.."}, {'m', "--"}, {'n', "-."},
+            {'o', "---"}, {'p', ".--."}, {'q', "--.-"}, {'r', ".-."}, {'s', "..."}, {'t', "-"}, {'u', "..-"},
+            {'v', "...-"}, {'w', ".--"}, {'x', "-..-"}, {'y', "-.--"}, {'z', "--.."}, {'0', "-----"},
+            {'1', ".----"}, {'2', "..---"}, {'3', "...--"}, {'4', "....-"}, {'5', "....."}, {'6', "-...."},
+            {'7', "--..."}, {'8', "---.."}, {'9', "----."} };
+
+        }
+
+        private void buttonVertalen_Click(object sender, EventArgs e)
+        {
+            //Nederlands naar morse code
+            if (textBoxNL.Text != null)
+            {
+                //textbox informatie pakken, kleine letters van maken en verwerken naar array
+                string TextNLString = textBoxNL.Text.ToLower();
+                char[] TextNL = TextNLString.ToArray();
+
+                //individuele characters bekijken en vertalen
+                StringBuilder BobdebouwerNL = new StringBuilder();
+                foreach (char Character in TextNL)
+                {
+                    if (VertalerNL.ContainsKey(Character))
+                    {
+                        BobdebouwerNL.Append(VertalerNL[Character] + " ");
+                    }
+                    else if (Character == ' ')
+                    {
+                        BobdebouwerNL.Append("/ ");
+                    }
+                    else
+                    {
+                        BobdebouwerNL.Append(" ");
+                    }
+                }
+                textBoxMC.Text += BobdebouwerNL.ToString();
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBoxMC.Text = "";
+            textBoxNL.Text = "";
+        }
+    }
+}
